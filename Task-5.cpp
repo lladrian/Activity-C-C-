88a7c32e259ad04cf3cdb55dd3e27d554bedba2e
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2022-04-10 22:12:08 -0800

    Update Task-5.cpp

--- Task-5.cpp
+++ Task-5.cpp
@@ -1,3 +1,15 @@
+/* 
+     Author : Adrian C. Manatad
+       Date : February 28, 2022
+     Reason : for commission
+     
+ Task 5: Create a program in C++ that will accept a number and display the cube of the accepted number. 
+Sample Output:
+Input the number of terms : 2
+Number is : 2 and the cube of 2 is: 8 
+
+*/
+
 #include <iostream>
 
 using namespace std;
