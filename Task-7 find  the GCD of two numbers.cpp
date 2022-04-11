a2146d389eb55ad77e80baa809792827a1a160cb
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2022-04-10 22:15:07 -0800

    Update and rename Task-7.cpp to Task-7 find  the GCD of two numbers.cpp

--- Task-7 find  the GCD of two numbers.cpp
+++ Task-7 find  the GCD of two numbers.cpp
@@ -1,3 +1,18 @@
+/* 
+     Author : Adrian C. Manatad
+       Date : February 28, 2022
+     Reason : for commission
+     
+Task 7: Create a program in C++ that will accept two number and find 
+the Greatest Common Divisor (GCD) of two numbers. 
+Sample Output:
+Input the first number: 20
+Input the second number: 10
+The Greatest Common Divisor is: 10
+
+*/
+
+
 #include <iostream>
 
 using namespace std;
