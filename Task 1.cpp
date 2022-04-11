403038a6885be06b60f049ad99bfe370954a5ea9
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2022-04-10 22:08:00 -0800

    Update Task 1.cpp

--- Task 1.cpp
+++ Task 1.cpp
@@ -1,3 +1,10 @@
+/* 
+     Author : Adrian C. Manatad
+       Date : February 28, 2022
+     Reason : for commission
+*/
+
+
 #include <iostream>
 
 using namespace std;
@@ -23,3 +30,11 @@ int num, key = 1;
     cout << "\n\n";
     return 0;
 }
+
+/*
+Task 1: Create a program in C++ that will accept a number and display 
+all factors of the number.
+Sample Output:
+Input a number: 21
+The factors are: 1 3 7 21
+*/
